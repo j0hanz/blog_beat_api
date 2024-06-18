d63632e0c13de031d1583c7d072b5b4f279aed16
Linus Johansson <golfarenlj@gmail.com>
2024-06-18 21:46:25 +0000

    Enhance ProfileList and ProfileDetail views with filtering and annotation

--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,43 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from django.http import Http404
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListCreateAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
 
     def get_object(self):
