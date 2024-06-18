0d0e7135ed80b246d72e9cd42d738e2d944a7460
Linus Johansson <golfarenlj@gmail.com>
2024-06-18 21:10:02 +0000

    Refactored code

--- profiles/views.py
+++ profiles/views.py
@@ -1,59 +1,23 @@
+from rest_framework import generics
 from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
-from blog_beat_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
-
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ProfileSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ProfileDetail(APIView):
+class ProfileList(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsOwnerOrReadOnly]
 
-    def get_object(self, pk):
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
         try:
-            return Profile.objects.get(pk=pk)
+            return super().get_object()
         except Profile.DoesNotExist:
             raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        self.check_object_permissions(request, profile)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        profile = self.get_object(pk)
-        self.check_object_permissions(request, profile)
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
