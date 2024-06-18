c897f98adc8e849263c8eccb35d2b138dbf463fa
Linus Johansson <golfarenlj@gmail.com>
2024-06-18 21:45:46 +0000

    Enhance ProfileSerializer with follower and post counts

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,15 +1,29 @@
 from rest_framework import serializers
-from .models import Profile
 from django_countries.serializers import CountryFieldMixin
+from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(CountryFieldMixin, serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
@@ -24,4 +38,8 @@ class ProfileSerializer(CountryFieldMixin, serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'is_owner',
+            'following_id',
+            'posts_count',
+            'followers_count',
+            'following_count',
         ]
