2546bd2612bc5dbf78f1902eed8f12c121519cc8
Linus Johansson <golfarenlj@gmail.com>
2024-06-19 11:57:45 +0000

    Add URL for BookmarkList and BookmarkDetail

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,6 @@ from posts import views
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
+    path('bookmarks/', views.BookmarkList.as_view()),
+    path('bookmarks/<int:pk>/', views.BookmarkDetail.as_view()),
 ]
