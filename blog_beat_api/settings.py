e7459b8773938b6d9d228a60ad4a7c3bb1ca1679
Linus Johansson <golfarenlj@gmail.com>
2024-06-23 10:39:31 +0000

    set ALLOWED_HOSTS

--- blog_beat_api/settings.py
+++ blog_beat_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
+    'blog-beat-api-bab609deb9ee.herokuapp.com',
     'localhost',
 ]
 
