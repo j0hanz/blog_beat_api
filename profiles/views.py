c14d709e37ba4a7b205287bc71dd6fe8fa0139b1
Linus Johansson <golfarenlj@gmail.com>
2024-06-24 22:39:43 +0000

    Add ListAPIView in ProfileList

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,14 @@
 from django.db.models import Count
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from django.http import Http404
 from .models import Profile
 from .serializers import ProfileSerializer
 from blog_beat_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(generics.ListCreateAPIView):
+class ProfileList(generics.ListAPIView):
     """
-    View for listing and creating profiles.
+    View for listing profiles.
     """
 
     queryset = Profile.objects.annotate(
@@ -37,19 +36,10 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     View for retrieving, updating, and deleting profiles.
     """
 
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__posts', distinct=True),
         followers_count=Count('owner__followers', distinct=True),
         following_count=Count('owner__following', distinct=True),
     ).order_by('-created_at')
-    serializer_class = ProfileSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self):
-        """
-        Retrieve the profile object, raising 404 if not found.
-        """
-        try:
-            return super().get_object()
-        except Profile.DoesNotExist:
-            raise Http404
