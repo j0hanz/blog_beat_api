c6bbaf9b320fc7a3ac65a90835c79f6c116306bc
Linus Johansson <golfarenlj@gmail.com>
2024-06-24 22:21:50 +0000

    Add path to URLs

--- blog_beat_api/urls.py
+++ blog_beat_api/urls.py
@@ -26,12 +26,11 @@ urlpatterns = [
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
-        'dj-rest-auth/registration/',
-        include('dj_rest_auth.registration.urls'),
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-    path('', include('profiles.urls')),
-    path('', include('posts.urls')),
-    path('', include('comments.urls')),
-    path('', include('likes.urls')),
-    path('', include('followers.urls')),
+    path('profiles/', include('profiles.urls')),
+    path('posts/', include('posts.urls')),
+    path('comments/', include('comments.urls')),
+    path('likes/', include('likes.urls')),
+    path('followers/', include('followers.urls')),
 ]
