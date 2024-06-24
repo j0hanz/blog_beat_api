56712996c9f78aed8308afebdb0250b89a3bdf06
Linus Johansson <golfarenlj@gmail.com>
2024-06-24 21:51:27 +0000

    Add favourite to URL

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view()),
-    path('posts/<int:pk>/', views.PostDetail.as_view()),
-    path('bookmarks/', views.BookmarkList.as_view()),
-    path('bookmarks/<int:pk>/', views.BookmarkDetail.as_view()),
+    path('posts/', views.PostList.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
+    path(
+        'posts/<int:pk>/favourite/',
+        views.AddRemoveFavourite.as_view(),
+        name='post-favourite',
+    ),
 ]
