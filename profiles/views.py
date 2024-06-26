0a4b364a0595705796b4799c111c05d8df1fa7bb
Linus Johansson <golfarenlj@gmail.com>
2024-06-26 19:49:27 +0000

    ix Profile queryset annotations and followers_count field annotation.

--- profiles/views.py
+++ profiles/views.py
@@ -13,11 +13,14 @@ class ProfileList(generics.ListAPIView):
 
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__posts', distinct=True),
-        followers_count=Count('owner__followers', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True),
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
     filterset_fields = [
         'owner__following__followed__profile',
         'owner__followed__owner__profile',
@@ -39,7 +42,7 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__posts', distinct=True),
-        followers_count=Count('owner__followers', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True),
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
