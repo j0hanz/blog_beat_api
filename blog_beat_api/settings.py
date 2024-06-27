45accc19b983ced93a11253522e7168bc0cc7b8e
Linus Johansson <golfarenlj@gmail.com>
2024-06-27 21:52:55 +0000

    Removed allauth from MIDDLEWARE. Downgraded to 0.54.0.

--- blog_beat_api/settings.py
+++ blog_beat_api/settings.py
@@ -123,7 +123,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'blog_beat_api.urls'
