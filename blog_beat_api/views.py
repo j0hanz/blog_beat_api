617d058b9dcf0bdef6f6aea095da6d0df2a67012
Linus Johansson <golfarenlj@gmail.com>
2024-06-28 10:06:58 +0000

    Cleanup code.

--- blog_beat_api/views.py
+++ blog_beat_api/views.py
@@ -1,39 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view()
 def root_route(request):
     return Response({"message": "Welcome to my Blog Beat API!"})
-
-
-# dj-rest-auth logout view fix
-'''
-@api_view(['POST'])
-def logout_route(request):
-    """
-    Log out the user by clearing the JWT auth and refresh cookies.
-    """
-    response = Response()
-    # Clear the JWT auth cookie
-    response.set_cookie(
-        key=JWT_AUTH_COOKIE,
-        value='',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
-    # Clear the JWT refresh cookie
-    response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
-        value='',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
-    return response
-'''
-
