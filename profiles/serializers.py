e8fc4694fdfdfae0d1529810dc8407c412843166
Linus Johansson <golfarenlj@gmail.com>
2024-07-02 23:39:45 +0000

    Enhance ProfileSerializer to support nested social media links creation and updates.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,6 +7,7 @@ from followers.models import Follower
 class SocialMediaLinkSerializer(serializers.ModelSerializer):
     """
     Serializer for the SocialMediaLink model.
+    This serializer validates URLs to ensure they start with "http://" or "https://".
     """
 
     def validate_url(self, value):
@@ -21,12 +22,13 @@ class SocialMediaLinkSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SocialMediaLink
-        fields = ['platform', 'url']
+        fields = ['id', 'platform', 'url']
 
 
 class ProfileSerializer(CountryFieldMixin, serializers.ModelSerializer):
     """
     Serializer for the Profile model.
+    This serializer handles creating and updating of profiles along with their nested social media links.
     """
 
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -55,6 +57,50 @@ class ProfileSerializer(CountryFieldMixin, serializers.ModelSerializer):
             ).first()
             return following.id if following else None
         return None
+
+    def create(self, validated_data):
+        """
+        Custom create method to handle nested social media links.
+        """
+        social_media_links_data = validated_data.pop('social_media_links', [])
+        profile = Profile.objects.create(**validated_data)
+        for link_data in social_media_links_data:
+            SocialMediaLink.objects.create(owner=profile.owner, **link_data)
+        return profile
+
+    def update(self, instance, validated_data):
+        """
+        Custom update method to handle nested social media links.
+        Updates profile fields and processes updates or creation of social media links.
+        """
+        social_media_links_data = validated_data.pop('social_media_links', [])
+
+        instance.first_name = validated_data.get(
+            'first_name', instance.first_name
+        )
+        instance.last_name = validated_data.get(
+            'last_name', instance.last_name
+        )
+        instance.country = validated_data.get('country', instance.country)
+        instance.bio = validated_data.get('bio', instance.bio)
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+
+        for link_data in social_media_links_data:
+            link_id = link_data.get('id', None)
+            if link_id:
+                link = SocialMediaLink.objects.get(
+                    id=link_id, owner=instance.owner
+                )
+                link.platform = link_data.get('platform', link.platform)
+                link.url = link_data.get('url', link.url)
+                link.save()
+            else:
+                SocialMediaLink.objects.create(
+                    owner=instance.owner, **link_data
+                )
+
+        return instance
 
     class Meta:
         model = Profile
