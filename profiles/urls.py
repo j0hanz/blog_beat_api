452644da6fa8142330ed80a1141532f0532ec375
Linus Johansson <golfarenlj@gmail.com>
2024-07-03 01:15:31 +0000

    Add path for, SocialMediaLinkList & SocialMediaLinkDetail.

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profiles/', views.ProfileList.as_view()),
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+    path('profiles/', views.ProfileList.as_view(), name='profile-list'),
+    path(
+        'profiles/<int:pk>/',
+        views.ProfileDetail.as_view(),
+        name='profile-detail',
+    ),
+    path(
+        'social-media-links/',
+        views.SocialMediaLinkList.as_view(),
+        name='social-media-link-list',
+    ),
+    path(
+        'social-media-links/<int:pk>/',
+        views.SocialMediaLinkDetail.as_view(),
+        name='social-media-link-detail',
+    ),
 ]
