97929d71a46b9b9a328b3b2a0bbb010f1167151b
Linus Johansson <golfarenlj@gmail.com>
2024-09-02 22:38:56 +0000

    Update root route response format

--- blog_beat_api/views.py
+++ blog_beat_api/views.py
@@ -4,4 +4,4 @@ from rest_framework.response import Response
 
 @api_view()
 def root_route(request):
-    return Response({"message": "Welcome to my Blog Beat API!"})
+    return Response({'message': 'Welcome to my Blog Beat API!'})
