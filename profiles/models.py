e1ef3bdf5005d406ca468d65f0a975159226946d
Linus Johansson <golfarenlj@gmail.com>
2024-09-03 13:49:16 +0000

    Update Profile model fields and add signal receiver

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django_countries.fields import CountryField
 
 
@@ -8,13 +9,13 @@ class Profile(models.Model):
     """Represents a user's profile with personal information and an image."""
 
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=255, blank=True)
-    last_name = models.CharField(max_length=255, blank=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
     country = CountryField(blank=True)
     bio = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/',
-        default='images/nobody.webp',
+        default='/static/images/nobody.webp',
         blank=True,
     )
     created_at = models.DateTimeField(auto_now_add=True)
@@ -27,10 +28,8 @@ class Profile(models.Model):
         return f"{self.owner}'s profile"
 
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs) -> None:
     """Function to create a profile once a user is created."""
     if created:
         Profile.objects.create(owner=instance)
-
-
-post_save.connect(create_profile, sender=User)
